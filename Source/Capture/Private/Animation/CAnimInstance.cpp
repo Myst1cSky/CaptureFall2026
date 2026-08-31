bb7e13f12de91ab588f351d5f1951b06620eaeb2
Jose Almendarez <skyfan1yt@gmail.com>
2026-08-31 16:31:22 -0500

    Added Animations to Character

--- Source/Capture/Private/Animation/CAnimInstance.cpp
+++ Source/Capture/Private/Animation/CAnimInstance.cpp
@@ -4,10 +4,12 @@
 #include "Animation/CAnimInstance.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UCAnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
+	OwningCharacter = Cast<ACharacter>(TryGetPawnOwner());
 	if (OwningCharacter)
 	{
 		OwningCharacterMovementComponent = OwningCharacter->GetCharacterMovement();
@@ -17,4 +19,20 @@ void UCAnimInstance::NativeInitializeAnimation()
 void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (OwningCharacter)
+	{
+		Speed = OwningCharacter->GetVelocity().Length();
+		
+		FRotator BodyRotation = OwningCharacter->GetActorRotation();
+		FRotator BodyRotationDelta = UKismetMathLibrary::NormalizedDeltaRotator(BodyRotation, BodyPrevRotation);
+		
+		YawSpeed = BodyRotationDelta.Yaw / DeltaSeconds;
+		SmoothedYawSpeed = UKismetMathLibrary::FInterpTo(SmoothedYawSpeed, YawSpeed, DeltaSeconds, YawSpeedSmoothLerpRate);
+		BodyPrevRotation = BodyRotation;
+	}
+	
+	if (OwningCharacterMovementComponent)
+	{
+		bIsFalling = OwningCharacterMovementComponent->IsFalling();
+	}
 }
--- Source/Capture/Private/Animation/CAnimInstance.h
+++ Source/Capture/Private/Animation/CAnimInstance.h
@@ -21,6 +21,14 @@ public:
 	// for the bulk of the work to be done in NativeThreadSafeUpdateAnimation.
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	
+	UFUNCTION(BlueprintCallable, meta =(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed > 0;}
+	
+	UFUNCTION(BlueprintCallable, meta =(BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return Speed == 0;}
+	
+	UFUNCTION(BlueprintCallable, meta =(BlueprintThreadSafe))
+	FORCEINLINE bool IsOnGround() const { return !bIsFalling; }
 private:
 	UPROPERTY()
 	class ACharacter* OwningCharacter;
@@ -30,4 +38,18 @@ private:
 	
 	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess=true))
 	float Speed;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+	bool bIsFalling;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+	float YawSpeed;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+	float SmoothedYawSpeed;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	float YawSpeedSmoothLerpRate = 2.f;
+	
+	FRotator BodyPrevRotation;
 };
