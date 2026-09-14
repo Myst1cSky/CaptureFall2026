0ce7f17ce1081afbef9336c0ac5e2bebfbe0492a
Jose Almendarez <skyfan1yt@gmail.com>
2026-09-14 17:46:13 -0500

    Added GA_Combo and started on Anim notify

---
+++ Source/Capture/Private/AbilitySystem/CAbilitySystemNativeTags.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_ABILITY_BASICATTACK);
+
+// this is a declaration only
+extern FNativeGameplayTag TAG_ABILITY_BASICATTACK;
--- Source/Capture/Private/Characters/CCharacter.cpp
+++ Source/Capture/Private/Characters/CCharacter.cpp
@@ -29,7 +29,7 @@ void ACCharacter::ServerSideInit()
 
 void ACCharacter::ClientSideInit()
 {
-	
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
 
 bool ACCharacter::IsLocallyControlledByPlayer() const
